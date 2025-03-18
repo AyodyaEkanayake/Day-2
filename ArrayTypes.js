878dcdfe67b7f3d4793c69625accbfbd44aa8f42
AyodyaEkanayake <ayodyae822@gmail.com>
2025-03-19 01:15:02 +0530

    Create ArrayTypes.js

---
+++ ArrayTypes.js
@@ -0,0 +1,45 @@
+//Integer array
+console.log("Integer Array")
+let nums = [2,4,6,8,10]
+console.log(nums)
+
+for(let j=0; j < nums.length; j++)
+{
+	console.log(j)
+}
+
+console.log("-------------------")
+console.log("String Array")
+
+//String array
+let fruits = ["Grapes", "Banana", "Papaya", "Mango" , "Orange" , "Guava"]
+console.log(fruits)
+
+for(let k=0; k < fruits.length; k++)
+{
+	console.log(k)
+}
+
+
+console.log("-------------------")
+console.log("Char Array")
+//Char array
+let vowels = ['a', 'e' , 'i' , 'o' , 'u']
+console.log(vowels)
+for(let m=0; m < vowels.length; m++)
+{
+	console.log(m)
+}
+
+
+console.log("-------------------")
+console.log("Double Array")
+
+//Double array
+let dubele = [12.4,3.4,6.7]
+console.log(dubele)
+
+for(let n=0; n < nums.length; n++)
+{
+	console.log(j)
+}
